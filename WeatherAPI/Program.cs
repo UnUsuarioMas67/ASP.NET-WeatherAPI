95915b874f2e0c63c58b35d4f0ca3508904ebfcd
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-07-04 19:47:31 -0400

    Add WeatherResult.cs

--- WeatherAPI/Program.cs
+++ WeatherAPI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 using Scalar.AspNetCore;
+using WeatherAPI;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -28,35 +29,38 @@ app.UseHttpsRedirection();
 // Load .env file
 Env.Load();
 
-// Get API key from environment variables
+// Get API key from .env file
 string apiKey = Environment.GetEnvironmentVariable("API_KEY")!;
 
 app.MapGet("/api/weather/{location}/{date1:datetime?}/{date2:datetime?}",
-        async Task<Results<Ok<object>, BadRequest, InternalServerError>> (string location, DateTime? date1, DateTime? date2, [FromQuery] string unitGroup = "metric",
+        async Task<Results<Ok<WeatherResult>, BadRequest, InternalServerError>>
+        (string location, DateTime? date1, DateTime? date2, [FromQuery] string unitGroup = "metric",
             [FromQuery] string lang = "en") =>
         {
-            date1 ??= DateTime.UtcNow;
-            date2 ??= DateTime.UtcNow.AddDays(14);
-            
+            date1 ??= DateTime.Now;
+            date2 ??= DateTime.Now.AddDays(14);
+
             var client = new HttpClient();
             var uri = new Uri(
-                $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date1.Value:yyyy-MM-dd}/{date2.Value:yyyy-MM-dd}?unitGroup={unitGroup}&lang={lang}&include=days%2Chours&key={apiKey}&contentType=json");
+                $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date1.Value:yyyy-MM-dd}/{date2.Value:yyyy-MM-dd}?unitGroup={unitGroup}&elements=datetime%2CdatetimeEpoch%2Cname%2Caddress%2CresolvedAddress%2Clatitude%2Clongitude%2Ctemp%2Cfeelslike%2Cdew%2Cprecip%2Cprecipprob%2Cprecipcover%2Cpreciptype%2Csnow%2Cwindspeed%2Cwinddir%2Cpressure%2Cconditions%2Cdescription%2Cicon&include=hours&key={apiKey}&contentType=json&lang={lang}"
+            );
+            
             var request = new HttpRequestMessage(HttpMethod.Get, uri);
 
             try
             {
                 var response = await client.SendAsync(request);
                 response.EnsureSuccessStatusCode();
-                var content = await response.Content.ReadAsStringAsync();
-                
-                var result = JsonSerializer.Deserialize<object>(content);
-                return TypedResults.Ok(result);
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var data = await response.Content.ReadFromJsonAsync<WeatherResult>(options);
+
+                return TypedResults.Ok(data);
             }
             catch (HttpRequestException e)
             {
                 if (e.StatusCode == HttpStatusCode.BadRequest)
                     return TypedResults.BadRequest();
-                
+
                 return TypedResults.InternalServerError();
             }
         })
---
+++ WeatherAPI/WeatherResult.cs
@@ -0,0 +1,63 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WeatherAPI;
+
+public class WeatherResult
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    [JsonPropertyName("resolvedAddress")] public string Address { get; set; }
+    public string Timezone { get; set; }
+    public double Tzoffset { get; set; }
+    public List<WeatherDay>? Days { get; set; }
+}
+
+public class WeatherDay
+{
+    [JsonPropertyName("datetime")] public DateOnly Date { get; set; }
+    
+    public double Temp { get; set; }
+    [JsonPropertyName("feelslike")] public double FeelsLike { get; set; }
+    public double Dew { get; set; }
+    
+    [JsonPropertyName("precip")] public double Precipitation { get; set; }
+    [JsonPropertyName("precipprob")] public double PrecipitationProbability { get; set; }
+    [JsonPropertyName("precipcover")] public double PrecipitationCover { get; set; }
+    [JsonPropertyName("preciptype")] public string[]? PrecipitationType { get; set; }
+    
+    public double Snow { get; set; }
+    
+    [JsonPropertyName("windspeed")] public double WindSpeed { get; set; }
+    [JsonPropertyName("winddir")] public double WindDirection { get; set; }
+
+    public double Pressure { get; set; }
+
+    public string Conditions { get; set; }
+    public string? Description { get; set; }
+    public string Icon { get; set; }
+
+    public List<WeatherHours> Hours { get; set; }
+}
+
+public class WeatherHours
+{
+    [JsonPropertyName("datetime")] public TimeOnly Time { get; set; }
+    
+    public double Temp { get; set; }
+    [JsonPropertyName("feelslike")] public double FeelsLike { get; set; }
+    public double Dew { get; set; }
+    
+    [JsonPropertyName("precip")] public double Precipitation { get; set; }
+    [JsonPropertyName("precipprob")] public double PrecipitationProbability { get; set; }
+    [JsonPropertyName("preciptype")] public string[]? PrecipitationType { get; set; }
+    
+    public double Snow { get; set; }
+    
+    [JsonPropertyName("windspeed")] public double WindSpeed { get; set; }
+    [JsonPropertyName("winddir")] public double WindDirection { get; set; }
+
+    public double Pressure { get; set; }
+
+    public string Conditions { get; set; }
+    public string Icon { get; set; }
+}
