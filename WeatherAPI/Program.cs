5bba09eae2be94881db88ced2e382eee87b58b4f
UnUsuarioMas67 <90117609+UnUsuarioMas67@users.noreply.github.com>
2025-07-03 20:26:04 -0400

    Initial commit

---
+++ WeatherAPI/Program.cs
@@ -0,0 +1,65 @@
+using System.Globalization;
+using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+const string apiKey = "HPBLYE9ZNT2US2HH8RP5DFP8X";
+
+app.MapGet("/api/weather/{location}/{date1:datetime?}/{date2:datetime?}",
+        async Task<Results<Ok<object>, BadRequest, InternalServerError>> (string location, DateTime? date1, DateTime? date2, [FromQuery] string unitGroup = "metric",
+            [FromQuery] string lang = "en") =>
+        {
+            date1 ??= DateTime.UtcNow;
+            date2 ??= DateTime.UtcNow.AddDays(14);
+            
+            var client = new HttpClient();
+            var uri = new Uri(
+                $"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{location}/{date1.Value:yyyy-MM-dd}/{date2.Value:yyyy-MM-dd}?unitGroup={unitGroup}&lang={lang}&include=days%2Chours&key={apiKey}&contentType=json");
+            var request = new HttpRequestMessage(HttpMethod.Get, uri);
+
+            try
+            {
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                
+                var result = JsonSerializer.Deserialize<object>(content);
+                return TypedResults.Ok(result);
+            }
+            catch (HttpRequestException e)
+            {
+                if (e.StatusCode == HttpStatusCode.BadRequest)
+                    return TypedResults.BadRequest();
+                
+                return TypedResults.InternalServerError();
+            }
+        })
+    .WithName("GetWeatherData")
+    .WithOpenApi(x => new OpenApiOperation(x)
+    {
+        Summary = "Get Weather Data",
+        Description = "Returns weather data from the Visual Crossing API",
+        Tags = new List<OpenApiTag> { new() { Name = "Visual Crossing" }, new() { Name = "Weather" } }
+    });
+
+app.Run();
